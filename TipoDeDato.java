59b081de04041f5ff79fe53209ec21086a3dd443
diazjimenezmarcos <diaz.jimenez.marcos@iescamas.es>
2025-09-18 19:22:56 +0200

    Add files via upload

---
+++ TipoDeDato.java
@@ -0,0 +1,16 @@
+package com;
+
+public class TipoDeDato {
+
+	public static void main(String[] args) {
+		int numero = 100;
+		float numero_decimal = 1.1F;
+		double numero_decimal2 = 1.1;
+		boolean logico = false;
+		String palabra = "Tomate";
+		char letra = 'd';
+		System.out.println("Numero" + 100);
+		System.out.println("Numero Decimal: " + numero_decimal);
+	}
+
+}
